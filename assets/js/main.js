99fbda7b12a43d93231285b3695e956c429028dc
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-04-03 23:52:39 +0200

    Messaggio in caso di vincita - Fine esercizio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,3 @@
-// Genero una funzione che mi crei un elemento nel DOM (decido io di che tipo, con quale classe e con quale informazione al suo interno)
-function createMyElement(typeOfElement, nameOfClass, innerElement){
-    //associo ad una variabile l'elemento creato
-    let elemento = document.createElement(typeOfElement)
-    //aggiungo una classe all'elemento
-    elemento.classList.add(nameOfClass)
-    //aggiungo un linea HTML dentro l'elemento creato
-    elemento.innerHTML = innerElement
-    //la funzione quando chiamata mi restituirà l'elemento creato con tutte le caratteristiche specificate dai parametri
-    return elemento
-}
-
 //salvo in una variabile la select
 const selectLabel = document.querySelector('#labelLevel')
 const select1 = document.querySelector('select')
@@ -39,15 +27,16 @@ button.addEventListener('click', function(){
     let counter = []
     //faccio partire un ciclo che mi generi gli elementi nel DOM
     for(let i = 1; i <=level; i++){
-        //stampo dentro l'elemento i div che associo ad una variabile"divBox"
+        //stampo dentro l'elemento i div che associo ad una variabile "divBox"
         const divBox = containerCampo.appendChild(createMyElement('div', `box-${numLevel}`, `${arrayNumbers[i-1]}`))
         //rendo cliccabili tutti gli elementi
         divBox.addEventListener('click', function(){
             if(arrayBombs.includes(arrayNumbers[i-1])){
+                //aggiungo la classe che da il background rosso e inserisco un'icona
                 this.classList.add('red')
                 this.innerHTML=`<i class="fa-solid fa-bomb fa-shake" style="color: #000000;"></i>`
-                console.log(this.innerText)
-                let main = document.querySelector('main')
+                console.log(this.innerText)//in console appare il numero contenuto nella cella cliccata
+                let main = document.querySelector('main')//seleziono il main e al click della bomba appare un messaggio
                 main.innerHTML+=`
                 <div class="position-absolute text-center">
                     <h1 class="text-uppercase text-danger" style="font-size: 10rem;">hai perso!!!</h1>
@@ -55,14 +44,24 @@ button.addEventListener('click', function(){
                 </div>
                 `
             }else{
+                //aggiungo la classe che da il background blue
                 this.classList.add('blue')
-                console.log(this.innerText)
+                console.log(this.innerText)//in console appare il numero contenuto nella cella cliccata
+                //aggiungo al click elementi all'array counter
                 counter.push(parseInt(this.innerText)/parseInt(this.innerText))
-                console.log(counter)
+                //se la lunghezza del counter è uguale a tutte le celle meno le 16 bombe allora appare il messaggio di vincita
+                if(counter.length == level-16){
+                    let main = document.querySelector('main')
+                    main.innerHTML+=`
+                        <div class="position-absolute text-center">
+                            <h1 class="text-uppercase text-success" style="font-size: 10rem;">hai vinto!!!</h1>
+                            <h2 class="text-uppercase text-success" style="font-size: 5rem;">hai totalizzato ${counter.length}   punti</h2>
+                        </div>
+                    `
+                }
             }
-        })
+        }) 
     }
-
 
     //faccio scomparire il pulsante Play 
     button.classList.add('d-none')
@@ -80,10 +79,7 @@ buttonReload.addEventListener('click', function(){
     window.location.reload()
 })
 
-
-
 /****************FUNCTION*****************/
-
 //questa funzione mi restituisce un array con lunghezza che posso decidere, contenente numeri random (intervallo da decidere) tutti diversi tra loro
 function randomNumberSolo(length,min,max){
     let array = []
@@ -101,4 +97,16 @@ function randomNumberSolo(length,min,max){
         z++
     }
     return array
+}
+
+// Questa funzione che crea un elemento nel DOM (decido io di che tipo, con quale classe e con quale informazione al suo interno)
+function createMyElement(typeOfElement, nameOfClass, innerElement){
+    //associo ad una variabile l'elemento creato
+    let elemento = document.createElement(typeOfElement)
+    //aggiungo una classe all'elemento
+    elemento.classList.add(nameOfClass)
+    //aggiungo un linea HTML dentro l'elemento creato
+    elemento.innerHTML = innerElement
+    //la funzione quando chiamata mi restituirà l'elemento creato con tutte le caratteristiche specificate dai parametri
+    return elemento
 }
